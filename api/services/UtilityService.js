1f4076e7a6599262e1c73c30e22739a304699c42
Bryan Gill <bryan@shipitweekly.com>
2016-02-13 23:09:54 -0600

    added logic to utility service, removed posts and users service.

--- api/services/UtilityService.js
+++ api/services/UtilityService.js
@@ -1,6 +1,9 @@
 /**
  * Created by bryangill on 1/16/16.
  */
+
+var rest = require('restling');
+
 var root = process.env.ROOT_URL;
 
 module.exports = {
@@ -8,5 +11,22 @@ module.exports = {
   getRoot: function () {
     "use strict";
     return root;
+  },
+
+  invoke: function(path, params) {
+    "use strict";
+
+    var url = this.getRoot() + path;
+
+    var options = {
+      query: params
+    };
+
+    try {
+      return rest.get(url, options);
+    } catch (error) {
+      sails.log.error(error);
+    }
+
   }
-}
+};
